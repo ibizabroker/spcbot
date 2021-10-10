2d6cbe5a77e1f73f2870164429160f1b9abc4c8d
tarun-gowda <tarun.gowda2000@gmail.com>
2021-10-10 14:58:59 +0530

    added embed

--- spcbot.py
+++ spcbot.py
@@ -18,33 +18,44 @@ gspread_client = gspread.authorize(credentials)
 Database = gspread_client.open("EC Database").worksheet("Database")
 
 list_usn = Database.col_values(Database.find("USN").col)
+list_name = Database.col_values(Database.find("NAME").col)
 list_backlog = Database.col_values(Database.find("Backlog").col)
 list_placed = Database.col_values(Database.find("Placement Status").col)
+
 list_usn.pop(0)
+list_name.pop(0)
 list_placed.pop(0)
 list_backlog.pop(0)
 
 list_usn_backlog = []
+list_name_backlog = []
 list_open_dream = []
+list_name_open_dream = []
 list_dream = []
+list_name_dream = []
 list_unplaced = []
+list_name_unplaced = []
 
 for i in range(len(list_usn)):
     if list_backlog[i] == "Yes":
         list_usn_backlog.append(list_usn[i])
+        list_name_backlog.append(list_name[i])
 
 for i in range(len(list_usn)):
     if list_placed[i] == "Open Dream":
         list_open_dream.append(list_usn[i])
+        list_name_open_dream.append(list_name[i])
     if list_placed[i] == "Dream":
         list_dream.append(list_usn[i])
+        list_name_dream.append(list_name[i])
     if list_placed[i] == "Unplaced":
         list_unplaced.append(list_usn[i])
+        list_name_unplaced.append(list_name[i])
 
 load_dotenv()
 token = os.getenv("token")
 
-discord_client = commands.Bot(command_prefix='-', help_command = None, case_insensitive = True)
+discord_client = commands.Bot(command_prefix = '-', help_command = None, case_insensitive = True)
 
 @discord_client.event
 async def on_ready():
@@ -53,7 +64,11 @@ async def on_ready():
 
 @discord_client.group(invoke_without_command=True)
 async def help(ctx):
-    embed = discord.Embed(title = 'Help', description = 'List of bot commands', color = discord.Color.green())
+    embed = discord.Embed(
+        title = 'Help', 
+        description = 'List of bot commands', 
+        colour = discord.Colour.green()
+    )
     embed.add_field(name = 'About Student Database', value = 'backlog, placed, unplaced, opendream, dream', inline = False)
     embed.add_field(name = 'Verify', value = 'verify, cutoff', inline = False)
     embed.set_footer(text = 'For more information on a command try -help <command>, ex: -help placed')
@@ -61,43 +76,71 @@ async def help(ctx):
 
 @help.command()
 async def backlog(ctx):
-    embed = discord.Embed(title = "Backlog", description = 'Returns a list of USNs of students with backlog', color = discord.Color.green())
+    embed = discord.Embed(
+        title = "Backlog", 
+        description = 'Returns a list of USNs of students with backlog', 
+        colour = discord.Colour.green()
+    )
     embed.add_field(name = '**Syntax**', value = '-backlog')
     await ctx.send(embed = embed)
 
 @help.command()
 async def placed(ctx):
-    embed = discord.Embed(title = "Placed", description = 'Returns a list of USNs of placed students', color = discord.Color.blue())
+    embed = discord.Embed(
+        title = "Placed", 
+        description = 'Returns a list of USNs of placed students', 
+        colour = discord.Colour.blue()
+    )
     embed.add_field(name = '**Syntax**', value = '-placed')
     await ctx.send(embed = embed)
 
 @help.command()
 async def unplaced(ctx):
-    embed = discord.Embed(title = "Unplaced", description = 'Returns a list of USNs of unplaced students', color = discord.Color.red())
+    embed = discord.Embed(
+        title = "Unplaced", 
+        description = 'Returns a list of USNs of unplaced students', 
+        colour = discord.Colour.red()
+    )
     embed.add_field(name = '**Syntax**', value = '-unplaced')
     await ctx.send(embed = embed)
 
 @help.command()
 async def opendream(ctx):
-    embed = discord.Embed(title = "Open Dream", description = 'Returns a list of USNs of students placed in Open Dream companies', color = discord.Color.blue())
+    embed = discord.Embed(
+        title = "Open Dream", 
+        description = 'Returns a list of USNs of students placed in Open Dream companies', 
+        colour = discord.Colour.blue()
+    )
     embed.add_field(name = '**Syntax**', value = '-opendream')
     await ctx.send(embed = embed)
 
 @help.command()
 async def dream(ctx):
-    embed = discord.Embed(title = "Dream", description = 'Returns a list of USNs of students placed in Dream companies', color = discord.Color.purple())
+    embed = discord.Embed(
+        title = "Dream", 
+        description = 'Returns a list of USNs of students placed in Dream companies', 
+        colour = discord.Colour.purple()
+    )
     embed.add_field(name = '**Syntax**', value = '-dream')
     await ctx.send(embed = embed)
 
 @help.command()
 async def verify(ctx):
-    embed = discord.Embed(title = "Verify", description = 'Returns a list of USNs of students who are already placed in a open dream or a dream company. Also returns a list of USNs of students who have backlogs and have applied for the given company.', color = discord.Color.green())
+    embed = discord.Embed(
+        title = "Verify", 
+        description = 'Returns a list of USNs of students who are already placed in a open dream or a dream company. Also returns a list of USNs of students who have backlogs and have applied for the given company.', 
+        colour = discord.Colour.green()
+    )
     embed.add_field(name = '**Syntax**', value = '-verify <attachment> Ex: -verify (attach a .xlsx file)')
     await ctx.send(embed = embed)
 
 @help.command()
 async def cutoff(ctx):
-    embed = discord.Embed(title = "Cutoff", description = 'Returns a list of USNs of students who are unplaced and are meeting the companies cutoff requirements, also returns a list of students placed in Dream companies who are eligible.', color = discord.Color.green())
+    embed = discord.Embed(
+        title = "Cutoff", 
+        description = '', 
+        colour = discord.Colour.green()
+    )
     embed.add_field(name = '**Syntax**', value = '-cutoff <cgpa> Ex: -cutoff 7.5')
     await ctx.send(embed = embed)
 
@@ -105,24 +148,166 @@ async def cutoff(ctx):
 async def hello(ctx):
     await ctx.send('Haaai ;)')
 
+def predicate(message, l, r):
+    def check(reaction, user):
+
+        if reaction.message.id != message.id or user == discord_client.user:
+            return False
+        if l and reaction.emoji == "◀":
+            return True
+        if r and reaction.emoji == "▶":
+            return True
+        return False
+
+    return check
+
 @discord_client.command()
 async def backlog(ctx):
-    await ctx.send(list_usn_backlog)
+    # embed = discord.Embed(
+    #     title = "Backlog Students",
+    #     description = '',
+    #     colour = discord.Colour.red() 
+    # )
+
+    # # string_usn_backlog = ''
+    # # string_name_backlog = ''
+    # # for i in range (len(list_usn_backlog)):
+    # #     string_usn_backlog += list_usn_backlog[i] + '\n'
+    # #     string_name_backlog += list_name_backlog[i] + '\n' 
+
+    # # embed.add_field(
+    # #     name = 'USN - Name',
+    # #     value = string_usn_backlog + '-' + string_name_backlog,
+    # #     inline = False
+    # # )
+    # for i in range (len(list_usn_backlog)):
+    #     embed.add_field(
+    #         name = '\u200b',
+    #         value = '{} - {}'.format(list_usn_backlog[i], list_name_backlog[i]),
+    #         # string_usn_backlog + '-' + string_name_backlog,
+    #         inline = False
+    #     )
+    
+    # embed.set_footer(text = 'count of students: {}'.format(len(list_name_backlog)))
+    # await ctx.send(embed = embed)
+    # await ctx.send(list_usn_backlog)
+    
+    composite_list = [list_usn_backlog[x:x + 20] for x in range(0, len(list_usn_backlog), 20)]
+    pages = []
+    for elements in composite_list:
+        string = ""
+        for element in elements:
+            string += element + "\n"
+        embed = discord.Embed(
+            title = "Backlog students",
+            colour = discord.Colour.red()
+        )
+        embed.add_field(name = "USNs", value = string)
+        pages.append(embed)
+
+    page = 0
+    left = "◀"
+    right = "▶"
+    while True:
+        msg = await ctx.send(embed = pages[(page)])
+        l = page != 0
+        r = page != len(pages) - 1
+        if l:
+            await msg.add_reaction(left)
+
+        if r:
+            await msg.add_reaction(right)
+
+        react = await discord_client.wait_for('reaction_add', check=predicate(msg, l, r))
+        
+        if str(react[0]) == left:
+            page -= 1
+        elif str(react[0]) == right:
+            page += 1
+
+        await msg.delete()
 
 @discord_client.command()
 async def opendream(ctx):
+    # embed = discord.Embed(
+    #     title = "Students placed in Open Dream companies",
+    #     description = '',
+    #     colour = discord.Colour.green()
+    # )
+
+    # for i in range (len(list_open_dream)):
+    #     embed.add_field(
+    #         name = '\u200b',
+    #         value = '{} - {}'.format(list_open_dream[i], list_name_open_dream[i]),
+    #         inline = False
+    #     )
+    
+    # embed.set_footer(text = 'count of students: {}'.format(len(list_name_open_dream)))
+    # await ctx.send(embed = embed)
+
     await ctx.send(list_open_dream)
 
 @discord_client.command()
 async def dream(ctx):
+    # embed = discord.Embed(
+    #     title = "Students placed in Dream companies",
+    #     description = '',
+    #     colour = discord.Colour.purple()
+    # )
+
+    # for i in range (len(list_dream)):
+    #     embed.add_field(
+    #         name = '\u200b',
+    #         value = '{} - {}'.format(list_dream[i], list_name_dream[i]),
+    #         inline = False
+    #     )
+    
+    # embed.set_footer(text = 'count of students: {}'.format(len(list_name_dream)))
+    # await ctx.send(embed = embed)
+
     await ctx.send(list_dream)
    
 @discord_client.command() 
 async def unplaced(ctx):
+    # embed = discord.Embed(
+    #     title = "Unplaced students",
+    #     description = '',
+    #     colour = discord.Colour.red()
+    # )
+
+    # for i in range (len(list_unplaced)):
+    #     embed.add_field(
+    #         name = '\u200b',
+    #         value = '{} - {}'.format(list_unplaced[i], list_name_unplaced[i]),
+    #         inline = False
+    #     )
+    
+    # embed.set_footer(text = 'count of students: {}'.format(len(list_name_unplaced)))
+    # await ctx.send(embed = embed)
+
     await ctx.send(list_unplaced)
 
 @discord_client.command()
 async def placed(ctx):
+    # list_placed_usn = list_open_dream + list_dream
+    # list_name_placed = list_name_open_dream + list_name_dream
+
+    # embed = discord.Embed(
+    #     title = "All placed students",
+    #     description = '',
+    #     colour = discord.Colour.blue()
+    # )
+
+    # for i in range (len(list_placed_usn)):
+    #     embed.add_field(
+    #         name = '\u200b',
+    #         value = '{} - {}'.format(list_placed_usn[i], list_name_placed[i]),
+    #         inline = False
+    #     )
+    
+    # embed.set_footer(text = 'count of students: {}'.format(len(list_name_placed)))
+    # await ctx.send(embed = embed)
+
     await ctx.send('Open Dream: {} \n\n Dream: {}'.format(list_open_dream, list_dream))
 
 @discord_client.command()
